28171dec61fc519f57e4f37d925662757730394f
Jannis Morgenstern <49486580+nyarthan@users.noreply.github.com>
2024-02-27 12:06:53 +0000

    feat: automatically discover new style entries (#9)

--- packages/css2tailwind/src/config.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const configSchema = z.object({
-  pluginGenerator: z.object({ stylesDirectory: z.string() }),
-});
-export type Config = z.infer<typeof configSchema>;
--- packages/css2tailwind/src/util.ts
+++
@@ -1,3 +0,0 @@
-export const kinds = ['components'] as const;
-export type Kind = (typeof kinds)[number];
-export type Kinds = typeof kinds;
